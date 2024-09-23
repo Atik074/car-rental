47505010299d98f9cfe87d3982761cf7c81662b1
Atik074 <atiksmats@gmail.com>
2024-09-23 22:51:55 +0600

    car rental site

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from '@nextui-org/react';
+
+
+const LoadingPage = () => {
+      return <Spinner label="Loading..." color="warning" />
+
+        
+    
+};
+
+export default LoadingPage;
